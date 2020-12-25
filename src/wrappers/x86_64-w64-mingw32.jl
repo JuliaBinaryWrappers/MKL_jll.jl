27158be9147d7522de018f93f7cc56edbc923967
jlbuild <juliabuildbot@gmail.com>
2020-12-25 01:55:19 +0000

    MKL_jll build 2021.1.1+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -2,73 +2,22 @@
 export libmkl_core, libmkl_rt
 
 using IntelOpenMP_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-LIBPATH_default = ""
-
-# Relative path to `libmkl_core`
-const libmkl_core_splitpath = ["bin", "mkl_core.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libmkl_core_path = ""
-
-# libmkl_core-specific global declaration
-# This will be filled out by __init__()
-libmkl_core_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libmkl_core = "mkl_core.dll"
-
-
-# Relative path to `libmkl_rt`
-const libmkl_rt_splitpath = ["bin", "mkl_rt.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libmkl_rt_path = ""
-
-# libmkl_rt-specific global declaration
-# This will be filled out by __init__()
-libmkl_rt_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libmkl_rt = "mkl_rt.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("MKL")
+JLLWrappers.@declare_library_product(libmkl_core, "mkl_core.1.dll")
+JLLWrappers.@declare_library_product(libmkl_rt, "mkl_rt.1.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"MKL")
+    JLLWrappers.@generate_init_header(IntelOpenMP_jll)
+    JLLWrappers.@init_library_product(
+        libmkl_core,
+        "bin\\mkl_core.1.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (IntelOpenMP_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (IntelOpenMP_jll.LIBPATH_list,))
+    JLLWrappers.@init_library_product(
+        libmkl_rt,
+        "bin\\mkl_rt.1.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libmkl_core_path = normpath(joinpath(artifact_dir, libmkl_core_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libmkl_core_handle = dlopen(libmkl_core_path)
-    push!(LIBPATH_list, dirname(libmkl_core_path))
-
-    global libmkl_rt_path = normpath(joinpath(artifact_dir, libmkl_rt_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libmkl_rt_handle = dlopen(libmkl_rt_path)
-    push!(LIBPATH_list, dirname(libmkl_rt_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
