4bb28d5ce454acdab3f5425623f65b5183df5bf3
jlbuild <juliabuildbot@gmail.com>
2024-04-22 14:07:03 +0000

    MKL_jll build 2024.1.0+0

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -2,11 +2,12 @@
 export libmkl_core, libmkl_rt
 
 using IntelOpenMP_jll
+using oneTBB_jll
 JLLWrappers.@generate_wrapper_header("MKL")
 JLLWrappers.@declare_library_product(libmkl_core, "mkl_core.2.dll")
 JLLWrappers.@declare_library_product(libmkl_rt, "mkl_rt.2.dll")
 function __init__()
-    JLLWrappers.@generate_init_header(IntelOpenMP_jll)
+    JLLWrappers.@generate_init_header(IntelOpenMP_jll, oneTBB_jll)
     JLLWrappers.@init_library_product(
         libmkl_core,
         "bin\\mkl_core.2.dll",
--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -2,11 +2,12 @@
 export libmkl_core, libmkl_rt
 
 using IntelOpenMP_jll
+using oneTBB_jll
 JLLWrappers.@generate_wrapper_header("MKL")
 JLLWrappers.@declare_library_product(libmkl_core, "libmkl_core.so.2")
 JLLWrappers.@declare_library_product(libmkl_rt, "libmkl_rt.so.2")
 function __init__()
-    JLLWrappers.@generate_init_header(IntelOpenMP_jll)
+    JLLWrappers.@generate_init_header(IntelOpenMP_jll, oneTBB_jll)
     JLLWrappers.@init_library_product(
         libmkl_core,
         "lib/libmkl_core.so",
